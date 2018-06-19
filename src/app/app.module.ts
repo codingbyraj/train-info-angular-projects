f48e1fdc522dfd8aa8be1888a20cceb6ca7077fb
Abhishek Raj <rai.abhishekraj@gmail.com>
2018-06-19 10:01:34 +0530

    files uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
-import { TodoComponent } from './todo/todo.component';
-import { TaskTxtboxComponent } from './task-txtbox/task-txtbox.component';
+import { LiveStatusComponent } from './live-status/live-status.component';
+import { LiveStatusService } from './services/live-status.service';
+import { RouterModule, Routes } from '@angular/router';
+import { PnrstatusComponent } from './pnrstatus/pnrstatus.component';
+import { TrainRouteComponent } from './train-route/train-route.component';
+import { SeatAvailabilityComponent } from './seat-availability/seat-availability.component';
+import { TrainFareComponent } from './train-fare/train-fare.component';
+import { TrainsBetweenStationsComponent } from './trains-between-stations/trains-between-stations.component';
 
+const appRoutes: Routes = [
+  
+  { path: 'livestatus', component: LiveStatusComponent },
+  { path: 'pnrstatus', component: PnrstatusComponent },
+  { path: 'trainroute', component: TrainRouteComponent },
+  { path: 'trainfare', component: TrainFareComponent },
+  { path: 'seatavailable', component: SeatAvailabilityComponent },
+  { path: 'trainsbetweenstations', component: TrainsBetweenStationsComponent },
+  
+  { path: '**', redirectTo: '/livestatus', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent,
-    TaskTxtboxComponent
+    LiveStatusComponent,
+    PnrstatusComponent,
+    TrainRouteComponent,
+    SeatAvailabilityComponent,
+    TrainFareComponent,
+    TrainsBetweenStationsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [LiveStatusService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/utils/api.ts
@@ -0,0 +1,2 @@
+//new api key - d9pk6io6im
+export const API = "5pf163qdia";
